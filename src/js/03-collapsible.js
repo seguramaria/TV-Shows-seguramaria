fa6116d215434abb8eaeba72c092f016e0c15ac0
MariaesSegura <msegber@gmail.com>
2020-07-09 22:59:07 +0200

    Add function to show collapsibles

---
+++ src/js/03-collapsible.js
@@ -0,0 +1,22 @@
+'use strict';
+// Función handler que muestra u oculta los collapsibles
+function showCollapsible(event) {
+  // Definición de variables
+  const favSection = document.querySelector('.js-list-fav');
+  const seriesSection = document.querySelector('.js-list-series');
+  const favTitle = document.querySelector('.favorite__series-title');
+  const seriesTitle = document.querySelector('.series-search-title');
+  const mainDocument = document.querySelector('.page__main');
+  if (event.target === favTitle) {
+    favSection.classList.remove('hidden');
+    seriesSection.classList.add('hidden');
+  } else if (event.target === seriesTitle) {
+    favSection.classList.add('hidden');
+    seriesSection.classList.remove('hidden');
+  } else if (event.target === mainDocument) {
+    favSection.classList.add('hidden');
+    seriesSection.classList.add('hidden');
+  }
+}
+
+document.addEventListener('click', showCollapsible);

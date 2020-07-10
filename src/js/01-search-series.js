4f63bb7e41669854823d395de724cb1611ee81a5
MariaesSegura <msegber@gmail.com>
2020-07-10 08:39:02 +0200

    Add function favorite select

--- src/js/01-search-series.js
+++ src/js/01-search-series.js
@@ -1,7 +1,4 @@
 'use strict';
-// Creamos una variable con un array vacío
-
-let series = [];
 
 // Declaramos la función para la búsqueda de series
 function searchTVSeries() {
@@ -10,7 +7,6 @@ function searchTVSeries() {
   // Hacemos la interpolación de cadenas en la url de la api. Hay que tener cuidado con la búsqueda. Es search, no singlesearch.
 
   const URL = `http://api.tvmaze.com/search/shows?q=${search}`;
-  console.log(URL);
 
   fetch(URL)
     .then((response) => response.json())
@@ -26,21 +22,53 @@ btn.addEventListener('click', searchTVSeries);
 
 // Función para pintar en el HTML los datos
 function printSeries(series) {
-  let ulSeries = '';
   for (const serie of series) {
-    if (serie.show.image.medium === null) {
-      const searchList = `<li class="serie"><h3 class="serie-name">${serie.show.name}</h3><div class="serie-img-container"><img class="serie-img" src="./assets/images/retro-tv.webp" alt="serie sin imagen" /></div></li>`;
-      ulSeries += searchList;
-      const seriesList = document.querySelector('.series__list');
-      seriesList.innerHTML = ulSeries;
-    } else {
-      const searchList = `<li class="serie"><h3 class="serie-name">${serie.show.name}</h3><div class="serie-img-container"><img class="serie-img" src="${serie.show.image.medium}" alt="imagen de serie" /></div></li>`;
-      ulSeries += searchList;
-      const seriesList = document.querySelector('.series__list');
-      seriesList.innerHTML = ulSeries;
-    }
+    let ul = document.querySelector('.series__list');
+
+    let seriesList = document.createElement('li');
+    ul.appendChild(seriesList);
+    seriesList.setAttribute('class', 'serie');
+    seriesList.addEventListener('click', selectFavoriteSerie);
+
+    let nameSerie = document.createElement('h3');
+    nameSerie.appendChild(document.createTextNode(serie.show.name));
+    nameSerie.setAttribute('class', 'serie-name');
+    seriesList.appendChild(nameSerie);
+
+    let imgSerie = document.createElement('img');
+    imgSerie.src = serie.show.image.medium;
+    imgSerie.alt = 'Imagen de serie';
+    imgSerie.setAttribute('class', 'serie-img');
+    seriesList.appendChild(imgSerie);
   }
 }
+
+// FUNCIÓN PARA AÑADIR LA CLASE FAV A TODAS LAS SERIES FAVORITAS
+
+function selectFavoriteSerie(event) {
+  event.currentTarget.classList.toggle('fav');
+}
+
+// if (serie.show.image.medium === null) {
+//   let ul = document.querySelector('.series__list');
+
+//   let seriesList = document.createElement('li');
+//   ul.appendChild(seriesList);
+//   seriesList.setAttribute('class', 'serie');
+//   seriesList.addEventListener('click', selectFavoriteSerie);
+
+//   let nameSerie = document.createElement('h3');
+//   nameSerie.appendChild(document.createTextNode(serie.show.name));
+//   nameSerie.setAttribute('class', 'serie-name');
+//   seriesList.appendChild(nameSerie);
+
+//   let imgSerie = document.createElement('img');
+//   imgSerie.src =
+//     'https://static.vecteezy.com/system/resources/previews/000/225/950/original/retro-television-set-vector.jpg';
+//   imgSerie.alt = 'Imagen de serie';
+//   imgSerie.setAttribute('class', 'serie-img');
+//   seriesList.appendChild(imgSerie);
+// }
 
 // function toggleSeriesResults() {
 //   const searchResult = document.querySelector('.search-result');
